4c33bfb99d7c51f61e59dd2f1d7be91d8fc78fdc
immutable-dev <immutable-org@protonmail.com>
2024-05-12 19:25:57 -0400

    redeploy

--- src/pages/api/_lib/puppeteer.ts
+++ src/pages/api/_lib/puppeteer.ts
@@ -1,15 +1,22 @@
 import { type Page } from "puppeteer-core";
-import puppeteer from "puppeteer-core";
 
 import chrome from "chrome-aws-lambda";
 let _page: Page | null;
 import { executablePath } from "puppeteer";
+
+let puppeteer: any;
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
 async function getPage() {
   if (_page) return _page;
   const options = {
     args: [
       // ...chrome.args,
+
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
@@ -19,8 +26,14 @@ async function getPage() {
     executablePath: executablePath(),
     headless: true,
   };
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options.executablePath = await chrome.executablePath;
+    options.args = chrome.args;
+  }
   console.log(options);
+  // @ts-ignore
   const browser = await puppeteer.launch(options);
+  // @ts-ignore
   _page = await browser.newPage();
   return _page;
 }
@@ -31,12 +44,15 @@ export async function getScreenshot(
   height: string | string[] | undefined,
 ) {
   const page = await getPage();
+  // @ts-ignore
   await page.goto(url);
+  // @ts-ignore
   await page.setViewport({
     width: Number(width) || 1280,
     height: Number(height) || 720,
     deviceScaleFactor: 2,
   });
+  // @ts-ignore
   const file = await page.screenshot();
   // await page.browser().close();
 
